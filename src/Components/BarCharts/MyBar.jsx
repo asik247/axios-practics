acf2ef387c16445ef398eeed3db2f9e94dbf5328
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 12:48:20 +0600

    obj desturcting

--- src/Components/BarCharts/MyBar.jsx
+++ src/Components/BarCharts/MyBar.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 
 const MyBar = ({singleData}) => {
     console.log(singleData);
-    const {name,section,} = singleData
+    // const {name,section,} = singleData
+
+    const {
+        id,name,class:classNam,section,
+
+        subjects:{
+            Physics, Chemistry, Math, Biology, English
+        }
+    }=singleData;
+
+  
     return (
         <div>
             <div>
-               <h1>{name}</h1>
-               <p>{section}</p>
+               {/* <h1>{name}</h1>
+               <p>{section}</p> */}
             </div>
         </div>
     );
