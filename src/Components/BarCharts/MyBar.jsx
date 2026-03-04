bf4cf20728ede5b065c1ddb2dcc24af382d281c9
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-04 13:09:01 +0600

    done all like Axios use chart and data leveling

--- src/Components/BarCharts/MyBar.jsx
+++ src/Components/BarCharts/MyBar.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
-import { BarChart } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, Tooltip, XAxis, YAxis } from 'recharts';
 
-const MyBar = ({singleData}) => {
+const MyBar = ({ singleData }) => {
     console.log(singleData);
     // const {name,section,} = singleData
 
-    const {
-        id,name,class:classNam,section,
+    // const {
+    //     id, name, class: classNam, section,
 
-        subjects:{
-            Physics, Chemistry, Math, Biology, English
-        }
-    }=singleData;
+    //     subjects: {
+    //         Physics, Chemistry, Math, Biology, English
+    //     }
+    // } = singleData;
 
-  
+
+
+
+
+
+
+    const chartData = [
+        {
+            subject: 'Physics',
+            total: singleData.subjects.Physics.total
+        },
+        {
+            subject: 'Chemistry',
+            total: singleData.subjects.Chemistry.total
+        },
+        {
+            subject: 'Math',
+            total: singleData.subjects.Math.total
+        },
+        {
+            subject: 'Biology',
+            total: singleData.subjects.Biology?.total || 0
+        },
+        {
+            subject: 'English',
+            total: singleData.subjects.English?.total || 0
+        },
+    ];
+
+
     return (
         <div>
             <div>
-              <BarChart width={500} height={500} data={}>
-
-              </BarChart>
+                <BarChart width={500} height={300} data={chartData}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="subject" />  {/* x-axis label */}
+                    {/* <YAxis />                     y-axis marks */}
+                    <Tooltip />                   {/* hover করলে value দেখাবে */}
+                    <Legend />                    {/* chart legend */}
+                    <Bar dataKey="total" fill="#8884d8" /> {/* bar height = total marks */}
+                </BarChart>
             </div>
         </div>
     );
